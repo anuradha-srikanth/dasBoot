98633e8c598f035d38b2f570a6791204b7ea36a9
Anu Srikanth <anuradha.srikanth@capitalone.com>
2019-02-13 16:46:58 -0500

    added shipwreck controller and front end views of app

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -7,13 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Hello world!
  *
  */
+
+// This annotation initializes many other annotations under the covers
+// One of the main subpackages allows Spring Boot to scan subprojects and, if Spring compatible, automatically configure them
 @SpringBootApplication
 public class App
 {
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
+    	// Starts Spring, sets up Spring context, sets up embedded container
     	SpringApplication.run(App.class, args);
     	
     }
 }
+
+
+
+// src/main/resources -> class path resource so when you refresh project and do a maven refresh, it will add the folder that you created to the project
+// public folder is a Spring Boot folder that stores static files
